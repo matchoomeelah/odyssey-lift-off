1bd119460e40914b3affb32694aa7f849718de4b
Matt Miller <matchoomeelah@gmail.com>
2024-04-10 16:50:07 -0700

    Part 2 complete

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Layout } from "../components";
+import QueryResult from "../components/query-result";
+import { gql, TypedDocumentNode, OperationVariables, useQuery } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { Track } from "../__generated__/graphql";
 
-/**
- * Tracks Page is the Catstronauts home page.
- * We display a grid of tracks fetched with useQuery with the TRACKS query
- */
+/** TRACKS query to retrieve all tracks */
+const TRACKS: TypedDocumentNode<any, OperationVariables> = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
+
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track: Track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
